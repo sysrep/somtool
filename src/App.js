daa447f46aacad8ac1e5401bbe595fdbd845b95e
Hung-Han Chen <chenhungh@gmail.com>
2015-10-19 15:42:25 +0300

    setup for drawing d3.hexin and somap

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import "../src/css/index.css"
 import "../node_modules/angular/angular.js"
 import "../node_modules/angular-ui-bootstrap/ui-bootstrap-tpls.js"
 import "../node_modules/angular-ui-router/build/angular-ui-router.js"
-import som from "../node_modules/som.js/dist/som.js"
 
+import "../node_modules/d3/d3.js"
+import "../src/d3.hexbin.js"
 import { config } from "../src/appConfig.js"
 import { mainController } from "../src/appControllers.js"
-
 
 // webpack-dev-server and
 // go to http://localhost:8080/webpack-dev-server/index.html
