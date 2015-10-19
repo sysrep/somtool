28a01c1070762e44df96089ce79dec275bf8b0c4
Hung-Han Chen <chenhungh@gmail.com>
2015-10-19 09:06:15 +0300

    switch to angularjs due to the time

--- src/App.js
+++ src/App.js
@@ -1,39 +1,4 @@
-import React, { Component } from 'react';
-import { NICE, SUPER_NICE } from './colors';
-
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { counter: 0 };
-    this.interval = setInterval(() => this.tick(), 1000);
-  }
-
-  tick() {
-    this.setState({
-      counter: this.state.counter + this.props.increment
+angular.module('somApp', [])
+    .controller('MainController', function() {
+        console.log('hello world')
     });
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  render() {
-    return (
-      <h1 style={{ color: this.props.color }}>
-        Counter ({this.props.increment}): {this.state.counter}
-      </h1>
-    );
-  }
-}
-
-export class App extends Component {
-  render() {
-    return (
-      <div>
-        <Counter increment={1} color={NICE} />
-        <Counter increment={5} color={SUPER_NICE} />
-      </div>
-    );
-  }
-}
